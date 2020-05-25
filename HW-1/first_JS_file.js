dcea561db91134e72502023703cc567972c82d84
AndriiAD <50865921+AndriiAD@users.noreply.github.com>
2020-05-25 18:32:17 +0300

    Add files via upload

---
+++ HW-1/first_JS_file.js
@@ -0,0 +1,81 @@
+//task 2
+// alert('Avramkin, it is!');
+// console.log('test the console from the script');
+
+// //task 3
+// let a =10;
+// let b = 5;
+
+// document.writeln('it is a-> ' + a + '; and it is b-> ' + b);
+
+// a=b;
+// document.write('here it is new a-> ' + a + '; and new b-> ' + b);
+
+
+
+// //task 4
+// let client = {};
+
+// let client = {
+//     name : "John Smith",
+//     age : 25,
+//     maried : true,
+//     email : undefined,
+//     phone : null
+// };
+// // somehow this pease of code giving an error. Can't understand why.
+// // errors: already declared; all undefined.
+// console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof maried);
+// console.log(typeof email);
+// console.log(typeof phone);
+
+
+
+// //task 5
+// let isAdult = confirm('Do you have 18 years old?');
+// alert(isAdult);
+
+
+
+//task 6
+let firstName = "Ivan";
+let surname = "Petrenko";
+let group = "510.js";
+let yearOfBirth = 1995;
+let merrigeStatus = false;
+let mail 
+let phoneNumber = null;
+
+console.log(yearOfBirth);
+console.log(merrigeStatus);
+console.log(firstName);
+console.log(surname);
+console.log(typeof mail);
+console.log(typeof phoneNumber);
+
+
+
+// //task 7
+// let yourLogin = prompt("What is your login?", "your.login");
+// let yourEmail = prompt("What is your email?", "your.email@mail.com");
+// let yourPassword = prompt("What is your Password?", "123456");
+// alert('Dear ' + yourLogin + ', your email is ' + yourEmail + ' and your password is ' + yourPassword);
+
+//task 8
+let second = 1;
+let minute = 60 * second;
+let hour = 60 * minute;
+let day = 24 * hour;
+let month = 31 * day;
+
+document.writeln('one hour having ' + hour + ' seconds');
+document.writeln('one day having ' + day + ' seconds');
+document.writeln('May having ' + month + ' seconds')
+
+// here I tried to write a script based on the feedback from user. I don't know how to take the data user entered.
+// let yourMonth = prompt("What month would you like to check?", "May");
+// alert('Month ' + yourMonth+ ' have ' + numberSeconds + ' seconds');
+
+// document.write('Month ' + yourMonth+ ' have ' + numberSeconds + ' seconds');
